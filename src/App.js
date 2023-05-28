381820ab5cab919b620d8c91cac0eee10e89c169
NicholasRevenco <nicholas.revenco.25@bishops.com>
2023-05-28 12:00:49 -0700

    Separate useState topic into own component

--- src/App.js
+++ src/App.js
@@ -1,44 +1,14 @@
 import './App.css';
 import { User } from "./User";
+import { UseState } from "./UseState";
+
 import { useState } from "react";
 
 function App() {
 
-  const users = [
-    //{name: "Pedro", age: 21}, 
-    //{ name: "Nicholas", age: 55 }, 
-    //{name: "Adrian", age: 23 }
-  ];
-
-  const [age, setAge] = useState(0);
-  const [inputValue, setInputValue] = useState("");
-  const [showText, setShowText] = useState(true);
-
-  const increaseAge = () => {
-    setAge(age + 1 );
-  }
-
-  const handleInputChange = (event) => {
-    setInputValue(event.target.value);
-  }
-
   return (
     <div className="App">
-
-        {users.map((user, key) => {
-          return <div> <User name={user.name} age={user.age} /> </div>
-        })}
-
-        { age }
-        <button onClick={increaseAge}>Increase Age</button>
-
-        
-        <input type="text" onChange={handleInputChange} />
-        {inputValue}
-
-        <button onClick={() => { setShowText(!showText)}}>Show Text</button>
-        {showText && <h1>hello</h1>}
-
+        <UseState />
     </div>
   );
 }
