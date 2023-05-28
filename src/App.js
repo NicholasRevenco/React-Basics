42191e893f68f4b2878f282641b5e34b57b74aa6
NicholasRevenco <nicholas.revenco.25@bishops.com>
2023-05-28 09:10:51 -0700

    Components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import './App.css';
 
 function App() {
-  const name = <h1>Nicholas</h1>;
   return (
     <div className="App">
-        {name}
+        <User />
     </div>
   );
 }
+
+const User = () => {
+  const name = <h1>Nicholas</h1>;
+  const age = <h2>16</h2>;
+  const email = <h2>nicholas.revenco@gmail.com</h2>;
+  const user = (
+    <div>
+      {name}
+      {age}
+      {email}
+    </div>
+  );
+  return <h1>{ user }</h1>;
+};
 
 export default App;
