507f8fa7a35c064ea07dfe3b3ee6b0228d8caa02
NicholasRevenco <nicholas.revenco.25@bishops.com>
2023-05-28 12:02:41 -0700

    Own CRUD component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { User } from "./User";
 import { UseState } from "./UseState";
+import { Crud } from "./Crud";
 
 import { useState } from "react";
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <div className="App">
         <UseState />
+        <Crud />
     </div>
   );
 }
---
+++ src/Crud.js
@@ -0,0 +1,6 @@
+import './App.css';
+import { useState } from "react";
+
+export const Crud = () => {
+    return <h1>hello</h1>
+}
