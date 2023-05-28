25a062bac210854a3cdcf399a5e55ff322655f49
NicholasRevenco <nicholas.revenco.25@bishops.com>
2023-05-28 12:21:54 -0700

    Add task small project

--- src/Crud.js
+++ src/Crud.js
@@ -2,5 +2,33 @@ import './App.css';
 import { useState } from "react";
 
 export const Crud = () => {
-    return <h1>hello</h1>
+
+    const [toDoList, setToDoList] = useState([]);
+    const [newTask, setNewTask] = useState("");
+
+    const handleChange = (event) => {
+        setNewTask(event.target.value);
+    };
+
+    const addTask = () => {
+        const newToDoList = [...toDoList, newTask];
+        setToDoList(newToDoList);
+        console.log(newToDoList);
+    }
+    
+    return (
+        <div className="App">
+            <div className="addTask">
+
+                <input onChange={handleChange} />
+                <button onClick={addTask}>Add Task</button>
+
+            </div>
+            <div className="list">
+                {toDoList.map((task, key) => {
+                    return <h1 key={key}>{task}</h1>
+                })}
+            </div>
+        </div>
+    )
 }
