f6ab0447d5f9204a72593aa557af0bbafa6bf790
NicholasRevenco <nicholas.revenco.25@bishops.com>
2023-05-28 11:01:38 -0700

    useState use

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import './App.css';
-import { User } from "./User"
+import { User } from "./User";
+import { useState } from "react";
 
 function App() {
 
   const users = [
-    {name: "Pedro", age: 21}, 
-    { name: "Nicholas", age: 55 }, 
-    {name: "Adrian", age: 23 }
+    //{name: "Pedro", age: 21}, 
+    //{ name: "Nicholas", age: 55 }, 
+    //{name: "Adrian", age: 23 }
   ];
+
+  const [age, setAge] = useState(0);
+  const [inputValue, setInputValue] = useState("");
+
+  const increaseAge = () => {
+    setAge(age + 1 );
+  }
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  }
 
   return (
     <div className="App">
@@ -15,6 +27,13 @@ function App() {
         {users.map((user, key) => {
           return <div> <User name={user.name} age={user.age} /> </div>
         })}
+
+        { age }
+        <button onClick={increaseAge}>Increase Age</button>
+
+        
+        <input type="text" onChange={handleInputChange} />
+        {inputValue}
 
     </div>
   );
