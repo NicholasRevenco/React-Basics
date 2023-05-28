cd107f7b3f3bb79a686bc6e8c69fb32ad19ed48e
NicholasRevenco <nicholas.revenco.25@bishops.com>
2023-05-28 09:15:27 -0700

    JSC props component

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        <User />
+        <User 
+          name="Nicholas"
+          age={21}
+          email="nicholas.revenco@gmail.com"
+        />
     </div>
   );
 }
 
-const User = () => {
-  const name = <h1>Nicholas</h1>;
-  const age = <h2>16</h2>;
-  const email = <h2>nicholas.revenco@gmail.com</h2>;
+const User = (props) => {
+  const name = <h1>{props.name}</h1>;
+  const age = <h2>{props.age}</h2>;
+  const email = <h2>{props.email}</h2>;
   const user = (
     <div>
       {name}
