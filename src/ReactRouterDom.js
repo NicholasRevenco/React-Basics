a4773789e16395c80d4e9a5a5e3aa0cbf65bbcdb
NicholasRevenco <nicholas.revenco.25@bishops.com>
2023-05-29 10:30:01 -0700

    React query

--- src/ReactRouterDom.js
+++ src/ReactRouterDom.js
@@ -4,31 +4,34 @@ import { Home } from './pages/Home';
 import { Menu } from './pages/Menu';
 import { Contact } from './pages/Contact';
 import { useState, createContext } from "react";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 export const AppContext = createContext();
-
-
 
 export const ReactRouterDom = () => {
     
     const [username, setUsername] = useState("Nicholas");
 
+    const client = new QueryClient();
+
     return (
         <div className="App">
             <AppContext.Provider value={{ username, setUsername }}>
-                <Router>
-                    <div>
-                        <Link to="/">Home</Link>
-                        <Link to="/menu">Menu</Link>
-                        <Link to="/contact">Contact</Link>
-                    </div>
-                    <Routes>
-                        <Route path="/" element={<Home />}/>
-                        <Route path="/menu" element={<Menu />}/>
-                        <Route path="/contact" element={<Contact />}/>
-                        <Route path="*" element={<h1>Page not found</h1>}/>
-                    </Routes>
-                </Router>
+                <QueryClientProvider client={client}>
+                    <Router>
+                        <div>
+                            <Link to="/">Home</Link>
+                            <Link to="/menu">Menu</Link>
+                            <Link to="/contact">Contact</Link>
+                        </div>
+                        <Routes>
+                            <Route path="/" element={<Home />}/>
+                            <Route path="/menu" element={<Menu />}/>
+                            <Route path="/contact" element={<Contact />}/>
+                            <Route path="*" element={<h1>Page not found</h1>}/>
+                        </Routes>
+                    </Router>
+                </QueryClientProvider>
             </ AppContext.Provider>
         </div>
     )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,28 @@
-import { ChangeProfile } from "../components/ChangeProfile";
 import { useContext } from "react";
 import { AppContext } from '../ReactRouterDom';
+import { useQuery } from "@tanstack/react-query";
 
 export const Home = () => {
 
+    const { data, isLoading, isError } = useQuery(["excuse"], () => {
+        return fetch("https://excuser-three.vercel.app/v1/excuse/")
+            .then((res) => res.json())
+            .then((data) => data[0].excuse ); 
+    });
+
     const { username } = useContext(AppContext);
+
+    if (isLoading) {
+        return <h1>The data is loading</h1>
+    }
+
+    if (isError) {
+        return <h1>Sorry, there was an error!</h1>
+    }
 
     return (
     <div>
         <h1>This is the home page and user is: {username}</h1>
+        <h1>{data}</h1>
     </div> )
 }
