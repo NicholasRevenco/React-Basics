08a32f4c87fb10b6b81b3d62fc3ab037dee7156e
NicholasRevenco <nicholas.revenco.25@bishops.com>
2023-05-28 22:07:29 -0700

    Excuse API

--- src/Api.js
+++ src/Api.js
@@ -1,32 +1,26 @@
 import './App.css';
 import { useState, useEffect } from "react";
-import Axios from "axios";
 
 export const Api = () => {
 
-    const [catFact, setCatFact] = useState("");
+    const [excuse, setExcuse] = useState("");
 
-    const fetchCatFact = () => {
+    const fetchExcuse = () => {
         fetch("https://excuser-three.vercel.app/v1/excuse/")
         .then((res) => res.json())
         .then((data) => {
-             console.log(data);
+             setExcuse(data[0].excuse);
     });
     }
 
     useEffect(() => {
-        fetchCatFact();
+        fetchExcuse();
     }, [])
-    fetch("https://excuser-three.vercel.app/v1/excuse/")
-        .then((res) => res.json())
-        .then((data) => {
-             console.log(data);
-    });
 
     return (
         <div className="App">
-            <p>{catFact}</p>
-            <button onClick={fetchCatFact}>Generate Cat Fact</button>
+            <p>{excuse}</p>
+            <button onClick={fetchExcuse}>Generate Cat Fact</button>
         </div>
     )
 }
